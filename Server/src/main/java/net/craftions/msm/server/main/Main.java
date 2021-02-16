d40c7246f6f4e3582309643d157a9baf804248e8
MCTzOCK <dev@mctzock.de>
2021-02-16 18:01:01 +0100

    Added RSA Library and basic Server code

--- Server/src/main/java/net/craftions/msm/server/main/Main.java
+++ Server/src/main/java/net/craftions/msm/server/main/Main.java
@@ -3,9 +3,19 @@
  */
 package net.craftions.msm.server.main;
 
+import net.craftions.msm.api.rsa.Keys;
+import net.craftions.msm.server.network.Server;
+
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        KeyPair kp = Keys.gen(2048);
+        Server.PRIVATE_KEY = kp.getPrivate();
+        Server.PUBLIC_KEY = kp.getPublic();
+        Server.create();
     }
 }
---
+++ Server/src/main/java/net/craftions/msm/server/network/Server.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.msm.server.network;
+
+import net.craftions.msm.api.rsa.Keys;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.Scanner;
+
+public class Server {
+
+    public static PrivateKey PRIVATE_KEY = null;
+    public static PublicKey PUBLIC_KEY = null;
+    public static Boolean needNewThread = true;
+
+    public static void create() throws IOException {
+        ServerSocket server = new ServerSocket(40800);
+        while(true) {
+            if(needNewThread){
+                // threaded client listener
+                Thread clientThread = new Thread(){
+                    @Override
+                    public void run() {
+                        try {
+                            // disable creation of empty threads
+                            needNewThread = false;
+                            Socket client = server.accept();
+                            System.out.println("Client connected from " + client.getRemoteSocketAddress());
+                            // enable thread creation
+                            needNewThread = true;
+                            // initialize i/o streams
+                            Scanner s = new Scanner(client.getInputStream());
+                            PrintWriter w = new PrintWriter(client.getOutputStream());
+                            // send current public key
+                            w.println(new String(PUBLIC_KEY.getEncoded()));
+                            String clientPubKeyRaw = s.nextLine();
+                            PublicKey clientPubKey = Keys.fromString(clientPubKeyRaw);
+                            while(s.hasNextLine()){
+                                String rawCommand = Keys.decrypt(s.nextLine().getBytes(StandardCharsets.UTF_8), PRIVATE_KEY);
+                                String[] args = rawCommand.split(" ");
+                                String command = args[0];
+                                if(command.equals("get-msm-info")){
+                                    if(args[1].equals("version")){
+                                        w.write(new String(Keys.encrypt("1.0", clientPubKey), StandardCharsets.UTF_8));
+                                    }else if(args[1].equals("name")){
+                                        w.write(new String(Keys.encrypt("Standard-Server", clientPubKey), StandardCharsets.UTF_8));
+                                    }
+                                }else if(command.equals("close")){
+                                    break;
+                                }
+                            }
+                            // close streams and free memory
+                            w.close();
+                            s.close();
+                            client.close();
+                            // !! deprecated !!
+                            currentThread().stop();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchAlgorithmException e) {
+                            e.printStackTrace();
+                        } catch (InvalidKeyException e) {
+                            e.printStackTrace();
+                        } catch (InvalidAlgorithmParameterException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchPaddingException e) {
+                            e.printStackTrace();
+                        } catch (BadPaddingException e) {
+                            e.printStackTrace();
+                        } catch (IllegalBlockSizeException e) {
+                            e.printStackTrace();
+                        }
+                        super.run();
+                    }
+                };
+                // start a new client listener
+                clientThread.start();
+            }
+        }
+    }
+}
