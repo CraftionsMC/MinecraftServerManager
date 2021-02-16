eb7a4bd3ad8c30de7ba453df3bb1ae192bd6bd77
MCTzOCK <dev@mctzock.de>
2021-02-16 16:32:52 +0100

    Created Main classes

---
+++ Client/src/main/java/net/craftions/msm/client/main/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021 Ben Siebert. All rights reserved.
+ */
+package net.craftions.msm.client.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
